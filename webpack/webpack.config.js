402383b9362d296c8ac0bef9179d1b05da4c20c2
Natan Altomar <natan.altomar14@gmail.com>
2022-04-29 19:46:29 -0300

    finalizado capítulo webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,15 +7,19 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = {
     mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
-    output:{
+    output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
     },
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
                 cache: true,
-                parallel: true,
+                parallel: true
             }),
             new OptimizeCSSAssetsPlugin({})
         ]
@@ -23,7 +27,6 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css"
-
         })
     ],
     module: {
@@ -31,10 +34,13 @@ module.exports = {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                // 'style-loader',
-                'css-loader',
-                'sass-loader'
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
