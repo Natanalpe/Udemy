ef9343e3465aa7fb898379f3549c6e64ab39f5b2
Natan Altomar <natan.altomar14@gmail.com>
2022-05-12 15:08:13 -0300

    update 12/05
    - Capítulo de Bootstrap finalizado
    - Iniciando capítulo de React

--- bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,18 +1,25 @@
 import $ from 'jquery'
 
+const loadHtmlSuccessCallbacks = []
+
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHtmlSuccessCallbacks.includes(callback)) {
+        loadHtmlSuccessCallbacks.push(callback)
+    }
+}
+
 function loadIncludes(parent) {
-    if (!parent) parent = 'body'
-    $(parent).find('[wm-include]').each(function (i, e) {
+    if(!parent) parent = 'body'
+    $(parent).find('[wm-include]').each(function(i, e) {
         const url = $(e).attr('wm-include')
         $.ajax({
             url,
             success(data) {
                 $(e).html(data)
-
-                // removendo pra ela nçao ser interpretada mais
-                // de uma vez
                 $(e).removeAttr('wm-include')
 
+                loadHtmlSuccessCallbacks.forEach(
+                    callback => callback(data))
                 loadIncludes(e)
             }
         })
